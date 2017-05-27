7e0fb31875b650e0ba9c7bd2a6d058fbfd744e92
Erin Hoops <ehoops@users.noreply.github.com>
2017-05-26 21:11:08 -0700

    Now connected to firebase to read data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './dogs_fort_funston.JPG';
 import './App.css';
+import HikeInput from './HikeInput';
+import * as firebase from 'firebase';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 7,
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref();
+    // set reference to part of the db to watch
+    const timeRef = rootRef.child('time');
+    // set real time listener
+    timeRef.on('value', snap => {
+      this.setState({
+        time: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +35,10 @@ class App extends Component {
         <p className="App-intro">
           Find and rate dog-friendly hikes in the Bay area.
         </p>
+        <HikeInput />
+        <h1>{this.state.time}</h1>
       </div>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
+
+let config = {
+  apiKey: "AIzaSyAhJGvcZr7s5d9dIXkvPayG4yf4eFc_Ei0",
+  authDomain: "dog-hikes.firebaseapp.com",
+  databaseURL: "https://dog-hikes.firebaseio.com",
+  projectId: "dog-hikes",
+  storageBucket: "dog-hikes.appspot.com",
+  messagingSenderId: "738078416726"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
